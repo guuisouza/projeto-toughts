2433cd72dcdc9e2a43b2a36a1b866a20bdd68c4e
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-09-17 15:19:58 -0300

    feat: Implements Tought and User models

---
+++ models/Tought.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize')
+
+const db = require('../db/connection')
+
+// user
+const User = require('../models/User')
+
+const Tought = db.define('Tought', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        require: true
+    }
+})
+
+Tought.belongsTo(User) // um pensamento pertence a um user
+User.hasMany(Tought) // um usuário tem muitos pensamentos
+
+module.exports = Tought
